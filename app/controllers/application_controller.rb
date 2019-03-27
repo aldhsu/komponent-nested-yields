5ff8c5f63b9a4759ebcddc919d1bdefa9941a299
Allen Hsu <allen.hsu@fivegoodfriends.com.au>
2019-03-27 19:14:18 +1000

    Update for perf test.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
   def index
     render inline: <<~ERB
-    <%= c "nested_yielder" do |ctx| %>
-      <%= ctx.inner_yield do %>
-        <div>Broken</div>
-      <% end %>
-    <% end %>
-
-    <%= c "nested_yielder" do |ctx| %>
-      <%= c "inner_yield" do %>
-        <div>Ok</div>
-      <% end %>
+    <% 400.times do %>
+      <%= c "nested_yielder" %>
     <% end %>
     ERB
   end
